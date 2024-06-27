232615e51b7841afff48e31d4407f7a85058ebc1
shansuvethan18 <shansuvethan5120@gmail.com>
2024-06-27 13:56:13 +0530

    seat and payment-2

--- app/movies/[movieId]/seats/page.jsx
+++ app/movies/[movieId]/seats/page.jsx
@@ -3,42 +3,69 @@ import React, { useState } from "react";
 import Image from "next/image";
 import Navbar from "@/app/components/navBar";
 import Link from "next/link";
+import "../seats/seatstyle.css";
 
 const Seats = () => {
   const [selectedSeats, setSelectedSeats] = useState([]);
+  const [seatHovered, setSeatHovered] = useState({});
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const handleMouseEnter = (section, side, rowIndex, seatIndex) => {
+    const seatKey = side
+      ? `${section}-${side}-${rowIndex}-${seatIndex}`
+      : `${section}-${rowIndex}-${seatIndex}`;
+    setSeatHovered((prev) => ({
+      ...prev,
+      [seatKey]: true,
+    }));
+  };
+
+  const handleMouseLeave = (section, side, rowIndex, seatIndex) => {
+    const seatKey = side
+      ? `${section}-${side}-${rowIndex}-${seatIndex}`
+      : `${section}-${rowIndex}-${seatIndex}`;
+    setSeatHovered((prev) => ({
+      ...prev,
+      [seatKey]: false,
+    }));
+  };
 
   const seats = {
     stalls: {
       left: Array.from({ length: 14 }, (_, rowIndex) =>
         Array.from({ length: 13 }, (_, seatIndex) => ({
-          number: `${String.fromCharCode(65 + rowIndex)}${seatIndex + 1}`,
+          number: `${seatIndex + 1}`,
           status: "available",
         }))
       ),
       right: Array.from({ length: 14 }, (_, rowIndex) =>
         Array.from({ length: 12 }, (_, seatIndex) => ({
-          number: `${String.fromCharCode(65 + rowIndex)}${seatIndex + 14}`,
+          number: `${seatIndex + 14}`,
           status: "available",
         }))
       ),
     },
     balcony: Array.from({ length: 6 }, (_, rowIndex) =>
       Array.from({ length: 20 }, (_, seatIndex) => ({
-        number: `${String.fromCharCode(65 + rowIndex)}${seatIndex + 1}`,
+        number: `${seatIndex + 1}`, //`${String.fromCharCode(65 + rowIndex)}${seatIndex + 1}`,
         status: "available",
       }))
     ),
   };
 
-  const handleSeatClick = (section, side, row, seat) => {
+  const handleSeatClick = (section, side, rowIndex, seatIndex) => {
     const seatKey = side
-      ? `${section}-${side}-${row}-${seat}`
-      : `${section}-${row}-${seat}`;
+      ? `${section}-${side}-${rowIndex}-${seatIndex}`
+      : `${section}-${rowIndex}-${seatIndex}`;
     setSelectedSeats((prev) =>
       prev.includes(seatKey)
         ? prev.filter((s) => s !== seatKey)
         : [...prev, seatKey]
     );
+  };
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
   };
 
   const renderSeats = (section, side) => {
@@ -50,22 +77,72 @@ const Seats = () => {
           </div>
         )}
         {row.map((seat, seatIndex) => {
-          const seatKey = `${section}-${side}-${rowIndex}-${seatIndex}`;
+          const seatKey = side
+            ? `${section}-${side}-${rowIndex}-${seatIndex}`
+            : `${section}-${rowIndex}-${seatIndex}`;
           const isSelected = selectedSeats.includes(seatKey);
+          const isHovered = seatHovered[seatKey];
+
           return (
             <div
               key={seatIndex}
-              className={`w-8 h-8 m-1 rounded-lg flex items-center justify-center cursor-pointer ${
-                isSelected ? "bg-green-500" : "bg-white"
-              }`}
-              onClick={() =>
-                handleSeatClick(section, side, rowIndex, seatIndex)
-              }
+              className={`w-8 h-8 m-1 rounded-lg flex items-center justify-center cursor-pointer bg-gray-100`}
               style={{ position: "relative" }}
             >
-              <Image src="/Seat.png" alt="seat" width={20} height={20} />
-              {isSelected && (
-                <span className="text-white text-xs absolute">
+              <svg
+                width="20"
+                height="23.33"
+                viewBox="0 0 30 35"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={() =>
+                  handleSeatClick(section, side, rowIndex, seatIndex)
+                }
+                onMouseEnter={() =>
+                  handleMouseEnter(section, side, rowIndex, seatIndex)
+                }
+                onMouseLeave={() =>
+                  handleMouseLeave(section, side, rowIndex, seatIndex)
+                }
+                className="seat-icon"
+              >
+                <path
+                  d="M3.74219 6C3.74219 2.68629 6.42848 0 9.74219 0H20.7422C24.0559 0 26.7422 2.68629 26.7422 6V24H3.74219V6Z"
+                  fill={isHovered || isSelected ? "#1CCE83" : "#CE1C1C"}
+                  className="seat-icon"
+                  // onMouseEnter={() =>
+                  //   handleMouseEnter(section, side, rowIndex, seatIndex)
+                  // }
+                  // onMouseLeave={() =>
+                  //   handleMouseLeave(section, side, rowIndex, seatIndex)
+                  // }
+                />
+                <rect
+                  x="3.74219"
+                  y="26"
+                  width="23"
+                  height="9"
+                  fill={isHovered || isSelected ? "#1CCE83" : "#CE1C1C"}
+                  className="seat-icon"
+                />
+                <rect
+                  x="24.7422"
+                  y="16"
+                  width="5"
+                  height="16"
+                  fill={isHovered || isSelected ? "#1CCE83" : "#CE1C1C"}
+                  className="seat-icon"
+                />
+                <rect
+                  x="0.742188"
+                  y="16"
+                  width="5"
+                  height="16"
+                  fill={isHovered || isSelected ? "#1CCE83" : "#CE1C1C"}
+                  className="seat-icon"
+                />
+              </svg>
+              {(isHovered || isSelected) && (
+                <span className="text-white font-semibold text-xs mb-2 absolute seat-icon">
                   {seat.number}
                 </span>
               )}
@@ -90,20 +167,58 @@ const Seats = () => {
         {row.map((seat, seatIndex) => {
           const seatKey = `balcony-${rowIndex}-${seatIndex}`;
           const isSelected = selectedSeats.includes(seatKey);
+          const isHovered = seatHovered[seatKey];
+
           return (
             <div
               key={seatIndex}
-              className={`w-8 h-8 m-1 rounded-lg flex items-center justify-center cursor-pointer ${
-                isSelected ? "bg-green-500" : "bg-white"
-              }`}
-              onClick={() =>
-                handleSeatClick("balcony", null, rowIndex, seatIndex)
-              }
+              className={`w-8 h-8 m-1 rounded-lg flex items-center justify-center cursor-pointer bg-gray-100`}
               style={{ position: "relative" }}
             >
-              <Image src="/Seats.svg" alt="seat" width={20} height={20} />
-              {isSelected && (
-                <span className="text-white text-xs absolute">
+              <svg
+                width="20"
+                height="23.33"
+                viewBox="0 0 30 35"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={() =>
+                  handleSeatClick("balcony", null, rowIndex, seatIndex)
+                }
+                onMouseEnter={() =>
+                  handleMouseEnter("balcony", null, rowIndex, seatIndex)
+                }
+                onMouseLeave={() =>
+                  handleMouseLeave("balcony", null, rowIndex, seatIndex)
+                }
+                className="seat-icon"
+              >
+                <path
+                  d="M3.74219 6C3.74219 2.68629 6.42848 0 9.74219 0H20.7422C24.0559 0 26.7422 2.68629 26.7422 6V24H3.74219V6Z"
+                  fill={isHovered || isSelected ? "#1CCE83" : "#CE1C1C"}
+                />
+                <rect
+                  x="3.74219"
+                  y="26"
+                  width="23"
+                  height="9"
+                  fill={isHovered || isSelected ? "#1CCE83" : "#CE1C1C"}
+                />
+                <rect
+                  x="24.7422"
+                  y="16"
+                  width="5"
+                  height="16"
+                  fill={isHovered || isSelected ? "#1CCE83" : "#CE1C1C"}
+                />
+                <rect
+                  x="0.742188"
+                  y="16"
+                  width="5"
+                  height="16"
+                  fill={isHovered || isSelected ? "#1CCE83" : "#CE1C1C"}
+                />
+              </svg>
+              {(isHovered || isSelected) && (
+                <span className="text-white text-xs mb-2 absolute">
                   {seat.number}
                 </span>
               )}
@@ -123,25 +238,78 @@ const Seats = () => {
       <section className="pt-20 md:pt-24 px-4">
         <div className="container pb-20 mx-auto">
           <div className="flex items-baseline justify-between space-x-20 mb-4">
-            <div className="relative">
-              <button className="bg-white border border-gray-400 rounded-lg px-4 py-2 shadow flex items-center space-x-2">
-                <span>14:40</span>
-                <svg
-                  className="w-4 h-4"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M19 9l-7 7-7-7"
-                  ></path>
-                </svg>
+            <div className="">
+              <button
+                className="bg-white border border-gray-400 rounded-lg px-4 py-2 shadow flex items-center space-x-2"
+                onClick={toggleDropdown}
+              >
+                <span className="font-semibold">14:40</span>
+                {isDropdownOpen ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    className="w-4 h-4 font-bold"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m4.5 15.75 7.5-7.5 7.5 7.5"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="w-4 h-4 font-bold"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19 9l-7 7-7-7"
+                    ></path>
+                  </svg>
+                )}
               </button>
+              <div className="">
+                {isDropdownOpen && (
+                  <div className="absolute mt-2 bg-white border w-1/4 px-10 py-3 border-gray-200 rounded-lg shadow-lg">
+                    <div className="grid grid-cols-4 gap-2 p-1">
+                      <button className="bg-gray-300 text-gray-700 py-2 px-0 font-bold text-xs rounded">
+                        07:00
+                      </button>
+                      <button className="bg-gray-300 text-gray-700 py-2 px-0 font-bold text-xs rounded">
+                        10:00
+                      </button>
+                      <button className="bg-red-700 text-white py-1 px-2 font-bold text-xs rounded">
+                        14:40
+                      </button>
+                      <button className="bg-white border border-gray-400 text-gray-700 py-2 px-0 font-bold text-xs rounded">
+                        18:35
+                      </button>
+                      <button className="bg-white border border-gray-400 text-gray-700 py-2 px-0 font-bold text-xs rounded">
+                        17:10
+                      </button>
+                      <button className="bg-white border border-gray-400 text-gray-700 py-2 px-0 font-bold text-xs rounded">
+                        15:40
+                      </button>
+                      <button className="bg-white border border-gray-400 text-gray-700 py-2 px-0 font-bold text-xs rounded">
+                        22:00
+                      </button>
+                      <button className="bg-white border border-gray-400 text-gray-700 py-2 px-0 font-bold text-xs rounded">
+                        19:00
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </div>
             </div>
+
             <div className="flex items-end">
               <div className="flex space-x-4">
                 <div className="flex items-center">
@@ -162,21 +330,22 @@ const Seats = () => {
 
           <div className="flex justify-center w-full items-start mb-2">
             <svg
-              viewBox="0 0 500 100"
+              width="929"
+              height="66"
+              viewBox="0 0 929 66"
+              fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              className="w-full h-20"
             >
               <path
-                d="M10 80 Q 250 20, 490 80"
-                stroke="#8B4A46"
-                strokeWidth="3"
-                fill="transparent"
+                d="M0.841056 62.7002C374.273 -17.0999 555.38 -13.8243 928.07 61.0082"
+                stroke="#8F5555"
+                stroke-width="6"
               />
             </svg>
           </div>
 
           <div className="mt-2 text-gray-500 text-center">Stalls seats</div>
-          <div className=" px-4 mt-4 overflow-x-auto">
+          <div className=" mt-4 overflow-x-auto">
             <div className="flex justify-center">
               <div className="mr-8">{renderSeats("stalls", "left")}</div>
               <div className="ml-8">{renderSeats("stalls", "right")}</div>
--- app/movies/[movieId]/seats/payment/page.jsx
+++ app/movies/[movieId]/seats/payment/page.jsx
@@ -11,27 +11,28 @@ function Payment() {
           <div className="bg-gray-100   p-8 flex-1 mr-16">
             <h2 className="text-2xl font-semibold mb-6">YOUR DETAILS</h2>
             <form className="space-y-6">
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Enter your Email Address
-                </label>
-                <input
-                  type="email"
-                  id="email"
-                  className="mt-1 block w-full p-3 border border-gray-300 rounded-md"
-                  placeholder="name@example.com"
-                />
+              <div className="relative">
+                <div className="relative mt-1">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="absolute inset-y-0 left-3  w-6 h-6 mt-3 text-black pointer-events-none"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="m20 8l-8 5l-8-5V6l8 5l8-5m0-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2"
+                    ></path>
+                  </svg>
+
+                  <input
+                    type="email"
+                    id="email"
+                    className="mt-1 block w-full p-3 pl-12 border border-gray-300 rounded-md"
+                    placeholder="Enter your Email Address*"
+                  />
+                </div>
               </div>
               <div>
-                <label
-                  htmlFor="phone"
-                  className="block text-sm font-medium text-gray-700 mb-1"
-                >
-                  Enter Your Phone Number
-                </label>
                 <div className="mt-1 flex">
                   <select
                     id="country-code"
@@ -40,46 +41,89 @@ function Payment() {
                     <option value="+94">+94</option>
                     {/* Add other country codes as needed */}
                   </select>
-                  <input
-                    type="text"
-                    id="phone"
-                    className="block w-full p-3 border border-gray-300 rounded-r-md"
-                    placeholder="123-456-7890"
-                  />
+                  <div className="relative w-full">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      className="absolute inset-y-0 left-3 mt-3 w-6 h-6 text-black pointer-events-none"
+                    >
+                      <g fill="none">
+                        <path
+                          fill="currentColor"
+                          d="M19 17v4c-2.758 0-5.07-.495-7-1.325c-3.841-1.652-6.176-4.63-7.5-7.675C3.4 9.472 3 6.898 3 5h4l1 4l-3.5 3c1.324 3.045 3.659 6.023 7.5 7.675L15 16z"
+                        ></path>
+                        <path
+                          stroke="currentColor"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M12 19.675c1.93.83 4.242 1.325 7 1.325v-4l-4-1zm0 0C8.159 18.023 5.824 15.045 4.5 12m0 0C3.4 9.472 3 6.898 3 5h4l1 4zM14 4h.01M17 4h.01M20 4h.01M14 7h.01M17 7h.01M20 7h.01M14 10h.01M17 10h.01M20 10h.01"
+                        ></path>
+                      </g>
+                    </svg>
+
+                    <input
+                      type="text"
+                      id="phone"
+                      className="block w-full p-3 pl-12 border border-gray-300 rounded-r-md"
+                      placeholder="Enter Your Phone Number*"
+                    />
+                  </div>
                 </div>
               </div>
             </form>
+
             <div className="mt-8 p-6 bg-white rounded-md">
               <div className="flex justify-between items-center mb-4">
-                <h3 className="text-lg font-semibold">
+                <h3 className="text-lg font-semibold w-1/2">
                   The Ministry of Ungentlemanly Warfare
                 </h3>
-                <p className="text-gray-600">16 Jan 2024, 14:40</p>
+                <div>
+                  <p className="text-gray-600 text-right">Time</p>
+                  <p className="text-sm font-semibold">16 Jan 2024, 14:40</p>
+                </div>
               </div>
-              <div className="flex justify-between items-center mb-2">
-                <p className="text-gray-600">Balcony Seats:</p>
-                <p className="text-gray-600">A1 B1 C1</p>
+              <div className="flex justify-between items-center w-1/2 mb-4">
+                <div className="flex space-x-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="1rem"
+                    height="1rem"
+                    viewBox="0 0 24 24"
+                    className="w-8 h-8 mb-1 text-black"
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M5 9.15V7c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2v2.16c-1.16.41-2 1.51-2 2.81V14H7v-2.04c0-1.29-.84-2.4-2-2.81M20 10c-1.1 0-2 .9-2 2v3H6v-3a2 2 0 1 0-4 0v5c0 1.1.9 2 2 2v2h2v-2h12v2h2v-2c1.1 0 2-.9 2-2v-5c0-1.1-.9-2-2-2"
+                    ></path>
+                  </svg>
+                  <div>
+                    <p className="text-gray-600 text-center  ">Balcony Seats</p>
+                    <p className="text-sm font-semibold ">A1 B1 C1</p>
+                  </div>
+                </div>
+
+                <div>
+                  <p className="text-gray-600 text-center  ">Stall Seats</p>
+                  <p className="text-sm font-semibold ">A1 B1 C1</p>
+                </div>
               </div>
-              <div className="flex justify-between items-center mb-2">
-                <p className="text-gray-600">Stall Seats:</p>
-                <p className="text-gray-600">A1 B1 C1</p>
+
+              <div className=" items-center mb-4">
+                <p className="text-gray-600">Seats Info</p>
+                <p className="font-bold">Full 5, Half 1</p>
               </div>
-              <div className="flex justify-between items-center mb-4">
-                <p className="text-gray-600">Seats Info:</p>
-                <p className="text-gray-600">Full 5, Half 1</p>
-              </div>
-              <div className="flex justify-between items-center mb-2">
-                <p className="font-bold text-gray-700">Booking Fees:</p>
-                <p className="font-bold text-gray-700">250.00</p>
+              <div className=" items-center mb-2">
+                <p className=" text-gray-600">Booking Fees:250.00</p>
               </div>
               <div className="flex justify-between items-center">
-                <p className="text-xl font-bold text-gray-900">Total:</p>
+                <p className="text-lg font-bold text-gray-900">Total:</p>
                 <p className="text-xl font-bold text-gray-900">5450.00</p>
               </div>
             </div>
             <div className="mt-8">
               <Link href={`/movies/id/seats/payment/success`} passHref>
-                <button className="w-full bg-purple-600 hover:bg-purple-700 text-white font-semibold py-3 rounded-md">
+                <button className="w-full bg-purple-500 hover:bg-purple-700 text-white font-semibold py-3 rounded-md">
                   PAYMENT
                 </button>
               </Link>
